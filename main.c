1b15e58ae3e4409658fe95f3fb79fcaa677bf25c
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-02-27 11:15:52 +0100

    working but i need to remove realloc :(

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 10:51:04 by dpalmese          #+#    #+#             */
-/*   Updated: 2024/02/25 17:57:51 by dpalmese         ###   ########.fr       */
+/*   Updated: 2024/02/27 11:14:55 by rizz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -14,9 +14,10 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 
-#define BUFFER_SIZE 1
+#define BUFFER_SIZE 1024
 
-char			*get_next_line(int fd);
+char	*get_next_line(int fd);
+char	*ft_strrealloc(char *ptr, size_t size);
 
 typedef struct	s_buffer
 {
@@ -26,11 +27,7 @@ typedef struct	s_buffer
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "You must pass a filename!\n");
-		return (-1);
-	}
-	char *filename = argv[1];
+	char *filename = "bible.txt";
 	int fd = open(filename, O_RDONLY);
 	if (fd < 0) {
 		perror("Error while opening the file");
@@ -41,38 +38,62 @@ int	main(int argc, char **argv)
 		printf("%s\n", line);
 		line = get_next_line(fd);
 	}
+	free(line);
 	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_buffer	buff = {.index = 0};
-	char			*result;
+	static t_buffer	buff= {.index = 0};
+	int				bytes_read;
+	char			*line;
 	char			*newline;
-	int				n;
-	char			*start;
-	
-	result = NULL;
-	start = buff.content + buff.index;
-	if (start == 0)
+	size_t			line_length;
+
+	line = NULL;
+	while (1)
 	{
-		n = read(fd, buff.content, BUFFER_SIZE);
-		if (n <= 0)
-			return (NULL);	
-	}
-	newline = ft_memchr(start, '\n', BUFFER_SIZE - buff.index);
-	while(newline == NULL) {
-		n = read(fd, buff.content, BUFFER_SIZE);
-		if (n <= 0)
+		if (buff.index == 0 || buff.index > BUFFER_SIZE)
+		{
+			bytes_read = read(fd, buff.content, BUFFER_SIZE);
+			buff.index = 0;
+		}
+		if (bytes_read <= 0)
 			return (NULL);
-		start = buff.content + buff.index;
-		result = ft_strrealloc(result, (ft_strlen(result) + BUFFER_SIZE - buff.index) + 1);
-		ft_memcpy(result + ft_strlen(result), start, BUFFER_SIZE - buff.index);
-		newline = ft_memchr(buff.content, '\n', n);
-		buff.index = 0;
+		newline = ft_memchr(buff.content + buff.index, '\n', BUFFER_SIZE - buff.index);
+		if (newline)
+		{
+			line_length = newline - (buff.content + buff.index);
+			line = realloc(line, ft_strlen(line) + line_length + 1);
+			ft_memcpy(line, (buff.content + buff.index), line_length);
+			buff.index += line_length + 1;
+			return (line);
+		}
+		else
+		{
+			line = realloc(line, ft_strlen(line) + (BUFFER_SIZE - buff.index) + 1);
+			ft_memcpy(line + ft_strlen(line), (buff.content + buff.index), (BUFFER_SIZE - buff.index));
+			buff.index = 0;
+		}
 	}
-	result = ft_strrealloc(result, (ft_strlen(result) + (newline - start)) + 1);
-	ft_memcpy(result + ft_strlen(result), start, newline - start);
-	buff.index = newline - buff.content;
-	return (result);
+}
+
+char	*ft_strrealloc(char *ptr, size_t size)
+{
+	char	*new_ptr;
+	if (size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (ptr == NULL)
+		return (ft_calloc(size, sizeof(char)));
+	new_ptr = ft_calloc(size, sizeof(char));
+	if (new_ptr)
+	{
+		ft_memcpy(new_ptr, ptr, ft_strlen(ptr));
+		free(ptr);	
+		return (new_ptr);
+	}
+	return (NULL);
 }
